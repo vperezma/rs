339ffcbe181f3c2bb44f0ea9ffe56a1aa7b5cc6f
vperezma <vperezma89@gmail.com>
2018-07-31 18:01:21 -0700

    feat(refactor): refactored everything

--- js/main.js
+++ js/main.js
@@ -1,27 +1,13 @@
-
-var timeOut;
-function scrollToTop() {
-  if (document.body.scrollTop!= 0 || document.documentElement.scrollTop!= 0){
-    window.scrollBy(0,-10);
-    timeOut=setTimeout('scrollToTop()',10);
-  }
-  else {
-  	clearTimeout(timeOut);
-  }
-}
-
-function scrollToId(id) {
-    id = id.replace("link", "");
-    $('html,body').animate({
-        scrollTop: $("#"+id).offset().top},
-        'slow');
-}
-function test(obj){
-$(".navbar-nav>li>a").click(function(e) {
-    e.preventDefault();
-    var id = obj.id;
-   $('html,body').animate({
-        scrollTop: $("#"+id).offset().top},
-        '2000');
+$(document).ready(function() {
+  $("a").on("click", function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate({
+          scrollTop: $(hash).offset().top
+        }, 500, function() {
+          window.location.hash = hash;
+        });
+    }
+  });
 });
-}
